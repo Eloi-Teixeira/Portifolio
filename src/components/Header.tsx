6f4f37d9242bf1d287948d8be5c110a87610104a
herokira <oline9473@gmail.com>
2025-02-15 02:04:13 -0500

    Enhance Header component with responsive design and language selection
    - Add mobile menu with toggle functionality
    - Implement language selection dropdown for desktop and mobile
    - Style header elements for improved layout and interactivity
    - Add responsive styles for mobile devices
    - Update main.css with new color variables and global styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,78 @@ import Logo from '../assets/imgs/Logo.tsx';
 import IconTranslate from '../assets/icons/IconTranslate.tsx';
 
 const Header = () => {
+
+  const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.currentTarget.id ;
+    console.log(target);
+    const menuContainer = document.querySelector(`#${target} ~ ul`);
+    menuContainer?.classList.toggle('show');
+  };
+
   return (
-    <header className='header-container'>
-      <nav className='header-content max-container'>
+    <header className="header-container">
+      <nav className="header-content max-container">
         <Logo />
-        <ul>
+        <ul className="menu-desktop-container">
           <li>
-            <button>
+            <button onClick={handleMenuClick} id='menu-lang-btn'>
               <IconTranslate />
             </button>
+            <ul className="language-container">
+              <li>
+                <input type="radio" name="language" id="portuguese" />
+                <label htmlFor="portuguese">Português</label>
+              </li>
+              <li>
+                <input type="radio" name="language" id="english" />
+                <label htmlFor="english">English</label>
+              </li>
+            </ul>
           </li>
-          <li><a href="#about">Sobre</a></li>
-          <li><a href="#project">Projetos</a></li>
-          <li><a href="#Contact">Contato</a></li>
+          <li>
+            <a href="#about" className='active'>Home</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#project">Projetos</a>
+          </li>
+          <li>
+            <a href="#Contact">Contato</a>
+          </li>
+        </ul>
+        <button id="menu-mobile-btn" onClick={handleMenuClick}>
+          <span className="menu-mobile-btn-bar"></span>
+        </button>
+        <ul className="menu-mobile-container">
+          <li>
+            <a href="#about" className='active'>Home</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+          <li>
+            <a href="#project">Projetos</a>
+          </li>
+          <li>
+            <a href="#Contact">Contato</a>
+          </li>
+          <li>
+            <button onClick={handleMenuClick} id='menu-lang-btn-mobile'>
+              <IconTranslate /> Traduzir
+            </button>
+            <ul className="language-container language-mobile-container">
+              <li>
+                <input type="radio" name="language" id="portuguese_mobile" />
+                <label htmlFor="portuguese_mobile">Português</label>
+              </li>
+              <li>
+                <input type="radio" name="language" id="english_mobile" />
+                <label htmlFor="english_mobile">English</label>
+              </li>
+            </ul>
+          </li>
         </ul>
       </nav>
     </header>
