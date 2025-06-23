452d6cf26905175402dc6b75e5aa5481cd59372a
herokira <oline9473@gmail.com>
2025-06-22 20:27:07 -0600

    Add internationalization to Intro component and update language files
    - Implement useTranslation hook in Intro component
    - Replace hardcoded text with translated strings
    - Add new translation keys to en.json and pt.json for Intro content
    - Update IntroTitle and DownloadButton translations

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,21 +2,21 @@ import IconGitHub from '../assets/icons/IconGitHub';
 import IconDownload from '../assets/icons/IconDownload';
 import ImgComputer from '../assets/imgs/computer.png';
 import Cv from '../assets/cv/cv.pdf';
+import { useTranslation } from 'react-i18next';
 
 const Intro = () => {
+  const { t } = useTranslation();
   return (
     <section className="max-container-2 intro-container">
       <div className="intro-content">
         <span>Elói Teixeira</span>
-        <h1>
-          Desenvolvedor <span>Full-Stack</span>
-        </h1>
+        <h1>{t('IntroTitle')}<span>Full-Stack</span></h1>
         <div>
           <a href="#" className="github-link" target="_blank">
             <IconGitHub />
           </a>
           <a className="download-cv" href={Cv} download>
-            Baixar CV
+            {t('DownloadButton')}
             <IconDownload />
           </a>
         </div>
